f065b4401c0cafc41596afa34b91d5bda41fd4ab
alien2021brain <sachinpandey262523@gmail.com>
2023-10-30 13:05:06 +0530

    new

---
+++ Lesson1/build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// const a = "sachin";
+// let b = 10;
+// let c = "6";
+// let ans = b / c;
+//  wright way
+let a = 10;
+let b = 20;
+const ans = a / b;
+console.log(ans);
---
+++ Lesson1/src/main.ts
@@ -0,0 +1,10 @@
+// const a = "sachin";
+// let b = 10;
+// let c = "6";
+// let ans = b / c;
+
+//  wright way
+let a: number = 10;
+let b: number = 20;
+const ans = a / b;
+console.log(ans);

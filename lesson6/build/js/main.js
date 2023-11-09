6799a0b0975356b478c96b65ff6752debb06f765
alien2021brain <sachinpandey262523@gmail.com>
2023-11-09 11:39:37 +0530

    "setget"

--- lesson6/build/js/main.js
+++ lesson6/build/js/main.js
@@ -1,18 +1,75 @@
 "use strict";
 class car {
-    constructor(name, price, serialno) {
+    constructor(name, price, serialno, model = "2023") {
         this.name = name;
         this.price = price;
         this.serialno = serialno;
+        this.model = model;
     }
     opt() {
         return this.serialno;
     }
 }
+const carobj = new car("Sachin", 10, 10);
+console.log("car", carobj.opt());
 class bike extends car {
+    constructor(color, name, price, serialno) {
+        super(name, price, serialno);
+        this.color = color;
+    }
     pricefun() {
-        return this.price;
+        return this.model;
     }
 }
-const obj = new bike("bmw", 2000000, 12345);
+const obj = new bike("black", "bmw", 2000000, 12345);
 console.log(obj.pricefun());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(actions) {
+        return `${this.name} ${actions} the ${this.instrument}`;
+    }
+}
+const page = new Guitarist("Jimmy", "guitar");
+console.log(page.play("play"));
+///////////////////////////////////////////////
+// now using the static variable
+class Peeps {
+    // decalered static function
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const Annu = new Peeps("Annu");
+const Sachin = new Peeps("Sachin");
+const Suruchi = new Peeps("Suruchi");
+console.log(Suruchi.id);
+// we can get the static varaible with help of peeps
+console.log(Peeps.getCount());
+///////////////////////////////////
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((ele) => typeof ele === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error("Params is not  an arry of string");
+    }
+}
+const Bandsobj = new Bands();
+Bandsobj.data = ["hii", "bye", "shy"];
+console.log(Bandsobj.data);
--- lesson6/src/main.ts
+++ lesson6/src/main.ts
@@ -10,6 +10,8 @@ class car {
     return this.serialno;
   }
 }
+const carobj = new car("Sachin", 10, 10);
+console.log("car", carobj.opt());
 class bike extends car {
   constructor(
     public color: string,
@@ -20,9 +22,68 @@ class bike extends car {
     super(name, price, serialno);
   }
 
-  public pricefun(): number {
-    return this.price;
+  public pricefun(): string {
+    return this.model;
   }
 }
 const obj = new bike("black", "bmw", 2000000, 12345);
 console.log(obj.pricefun());
+
+// using interface with class
+interface Musican {
+  name: string;
+  instrument: string;
+  play(actions: string): string;
+}
+
+class Guitarist implements Musican {
+  constructor(public name: string, public instrument: string) {}
+  play(actions: string): string {
+    return `${this.name} ${actions} the ${this.instrument}`;
+  }
+}
+const page = new Guitarist("Jimmy", "guitar");
+console.log(page.play("play"));
+
+///////////////////////////////////////////////
+
+// now using the static variable
+class Peeps {
+  static count: number = 0;
+  // decalered static function
+  static getCount(): number {
+    return Peeps.count;
+  }
+  public id: number;
+  constructor(public name: string) {
+    this.id = ++Peeps.count;
+  }
+}
+
+const Annu = new Peeps("Annu");
+const Sachin = new Peeps("Sachin");
+const Suruchi = new Peeps("Suruchi");
+console.log(Suruchi.id);
+// we can get the static varaible with help of peeps
+console.log(Peeps.getCount());
+
+///////////////////////////////////
+
+class Bands {
+  private dataState: string[];
+  constructor() {
+    this.dataState = [];
+  }
+  public get data(): string[] {
+    return this.dataState;
+  }
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((ele) => typeof ele === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("Params is not  an arry of string");
+  }
+}
+const Bandsobj = new Bands();
+Bandsobj.data = ["hii", "bye", "shy"];
+console.log(Bandsobj.data);
